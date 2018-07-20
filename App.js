9a384bd92cfcd16b10138695af42af09864b230d
Niko Ruotsalainen <niko.ruotsalainen@newthings.co>
2018-07-20 16:32:28 +0300

    Refactor

--- App.js
+++ App.js
@@ -19,37 +19,34 @@ export default class CameraExample extends React.Component {
 
   render() {
     const { hasCameraPermission, faces } = this.state
-    if (hasCameraPermission === null) {
-      return <View />
-    } else if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>
-    } else {
-      return (
-        <View style={{ flex: 1 }}>
-          <Camera
-            style={{ flex: 1 }}
-            type={Camera.Constants.Type.front}
-            faceDetectionMode={Camera.Constants.FaceDetection.Mode.fast}
-            faceDetectionLandmarks={Camera.Constants.FaceDetection.Landmarks.none}
-            faceDetectionClassifications={Camera.Constants.FaceDetection.Classifications.none}
-            onFacesDetected={this.onFacesDetected.bind(this)}
-          />
-          {
-            faces.map(({bounds: {origin, size}, smilingProbability}, index) =>
-              <View key={index} style={{
-                position: 'absolute',
-                top: origin.y,
-                left: origin.x,
-                width: size.width,
-                height: size.height,
-                borderWidth: 1,
-                borderColor: 'yellow',
-                borderStyle: 'solid'
-              }} />
-            )
-          }
-        </View>
-      )
-    }
+    if (hasCameraPermission === null) return <View />
+    if (hasCameraPermission === false) return <Text>No access to camera</Text>
+
+    return (
+      <View style={{ flex: 1 }}>
+        <Camera
+          style={{ flex: 1 }}
+          type={Camera.Constants.Type.front}
+          faceDetectionMode={Camera.Constants.FaceDetection.Mode.fast}
+          faceDetectionLandmarks={Camera.Constants.FaceDetection.Landmarks.none}
+          faceDetectionClassifications={Camera.Constants.FaceDetection.Classifications.none}
+          onFacesDetected={this.onFacesDetected.bind(this)}
+        />
+        {
+          faces.map(({bounds: {origin, size}}, index) =>
+            <View key={index} style={{
+              position: 'absolute',
+              top: origin.y,
+              left: origin.x,
+              width: size.width,
+              height: size.height,
+              borderWidth: 1,
+              borderColor: 'yellow',
+              borderStyle: 'solid'
+            }} />
+          )
+        }
+      </View>
+    )
   }
 }
